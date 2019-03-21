794f4cc183cc8ca8ccb780f5aa4c09adbc12b33c
houfei <331488738@qq.com>
2019-03-22 00:07:26 +0800

    TCP通讯

--- src/chat/Client.java
+++ src/chat/Client.java
@@ -1,8 +1,6 @@
 package chat;
 
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
 import java.util.Scanner;
 
@@ -21,14 +19,31 @@ public class Client {
     public void start(){
         try{
             Scanner scanner = new Scanner(System.in);
+            // 先要求用户输入一个昵称
+            String nickName = null;
+            while(true){
+                System.out.println("请输入用户名：");
+                nickName = scanner.nextLine();
+                if (nickName.length()>0){
+                    return;
+                }
+                System.out.println("输入有误！！");
+            }
+            System.out.println("欢迎你" + nickName + "! 开始聊天吧!");
             OutputStream out = socket.getOutputStream();
             OutputStreamWriter osw = new OutputStreamWriter(out,"UTF-8");
             PrintWriter pw = new PrintWriter(osw,true);
+            // 先将昵称发送给服务器端
+            pw.println(nickName);
+
+            // 启动线程
+            ServerHandler handler = new ServerHandler();
+            Thread t = new Thread(handler);
+            t.start();
             while(true){
                 // 将字符串发送至服务端
                 pw.println(scanner.nextLine());
             }
-
         }catch(Exception e){
             e.printStackTrace();
         }
@@ -40,6 +55,24 @@ public class Client {
         }catch(Exception e){
             e.printStackTrace();
             System.out.println("客户端启动失败！！");
+        }
+    }
+    // 用来读取服务端发来的消息
+    class ServerHandler implements Runnable{
+
+        @Override
+        public void run() {
+            try{
+                InputStream in = socket.getInputStream();
+                InputStreamReader isr = new InputStreamReader(in,"UTF-8");
+                BufferedReader br = new BufferedReader(isr);
+                String message = null;
+                while((message = br.readLine()) != null){
+                    System.out.println(message);
+                }
+            }catch(Exception e){
+
+            }
         }
     }
 }
--- src/chat/Server.java
+++ src/chat/Server.java
@@ -1,11 +1,11 @@
 package chat;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 聊天室服务端
@@ -13,9 +13,26 @@ import java.net.Socket;
 public class Server {
     // 运行在服务端的ServerSocket主要负责：1.向系统申请服务端口 2.监听申请的服务端口
     private ServerSocket server;
+    // 保存所有客户端输出流的集合
+    private List<PrintWriter> allOut;
     public Server()throws Exception{
         // 初始化的同时申请服务端口
         server = new ServerSocket(8198);
+        allOut = new ArrayList<PrintWriter>();
+    }
+    // 将给定的输出流存入共享集合
+    private synchronized void addOut(PrintWriter out){
+        allOut.add(out);
+    }
+    // 将给定的输出流从共享集合中删除
+    private synchronized void removeOut(PrintWriter out){
+        allOut.remove(out);
+    }
+    // 将给定的消息发给所有客户端
+    private synchronized void sendMessage(String message){
+        for (PrintWriter out: allOut) {
+            out.println(message);
+        }
     }
     // 服务端开始方法
     public void start(){
@@ -52,6 +69,9 @@ public class Server {
         private Socket socket;
         // 客户端的地址信息
         private String host;
+        // 昵称
+        private String nickName;
+
         public ClientHandler(Socket socket){
             this.socket = socket;
             // 通过sockt可以获取计算机的地址信息
@@ -61,21 +81,36 @@ public class Server {
         }
         @Override
         public void run() {
+            PrintWriter pw = null;
             try{
-                System.out.println(host + "上线了！");
                 // 获取输入流，读取远程发送过来的消息
                 InputStream in = socket.getInputStream();
                 InputStreamReader isr = new InputStreamReader(in,"UTF-8");
                 BufferedReader br = new BufferedReader(isr);
+                // 首先读取一串字符串
+                nickName = br.readLine();
+                sendMessage(nickName + "上线了！！");
+                // 通过socket创建输出流用于把消息发给客户端
+                OutputStream out = socket.getOutputStream();
+                OutputStreamWriter osw = new OutputStreamWriter(out,"UTF-8");
+                pw = new PrintWriter(osw,true);
+                // 将该客户端的输出流存入到共享集合中去
+                addOut(pw);
                 String message = null;
                 while((message = br.readLine()) != null){
-                    System.out.println(host + "说：" + message);
+                  //  System.out.println(host + "说：" + message);
+                  //  pw.println(host + "说：" + message);
+                    // 广播消息
+                    sendMessage(nickName + "说：" + message);
                 }
             }catch(Exception e){
                 e.printStackTrace();
             } finally {
+                // 将该客户端的输出流从共享集合中删除
+                removeOut(pw);
                 // 处理当前客户端断开后的逻辑
-                System.out.println(host + "下线了！");
+                // System.out.println(host + "下线了！");
+                sendMessage(nickName + "下线了！！");
                 try{
                     socket.close();
                 }catch(Exception e){
