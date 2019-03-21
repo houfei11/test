dfa473c043bd527359c926f2cea2fc8b33432418
houfei <331488738@qq.com>
2019-03-21 21:32:42 +0800

    TCP通讯

--- src/chat/Client.java
+++ src/chat/Client.java
@@ -1,6 +1,10 @@
 package chat;
 
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 /**
  * 聊天室客户端
@@ -10,12 +14,24 @@ public class Client {
     private Socket socket;
     public Client()throws Exception{
         System.out.println("正在连接服务端。。。");
-        socket = new Socket("localhost",8088);
+        socket = new Socket("localhost",8198);
         System.out.println("服务端连接成功！");
     }
     // 启动客户端的方法
     public void start(){
+        try{
+            Scanner scanner = new Scanner(System.in);
+            OutputStream out = socket.getOutputStream();
+            OutputStreamWriter osw = new OutputStreamWriter(out,"UTF-8");
+            PrintWriter pw = new PrintWriter(osw,true);
+            while(true){
+                // 将字符串发送至服务端
+                pw.println(scanner.nextLine());
+            }
 
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
     public static void main(String[] args) {
         try{
--- src/chat/Server.java
+++ src/chat/Server.java
@@ -1,5 +1,9 @@
 package chat;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -11,15 +15,22 @@ public class Server {
     private ServerSocket server;
     public Server()throws Exception{
         // 初始化的同时申请服务端口
-        server = new ServerSocket(8088);
+        server = new ServerSocket(8198);
     }
     // 服务端开始方法
     public void start(){
         try{
             // ServerSocket的accept方法是一个阻塞方法，作用是监听服务端口
-            System.out.println("等待客户端连接。。。");
-            Socket socket = server.accept();
-            System.out.println("连接成功！");
+            while(true){
+                System.out.println("等待客户端连接。。。");
+                Socket socket = server.accept();
+                System.out.println("连接成功！");
+                // 启动一个线程，来完成客户端的交互工作
+                ClientHandler handler = new ClientHandler(socket);
+                Thread t = new Thread(handler);
+                t.start();
+            }
+
         }catch(Exception e){
             e.printStackTrace();
         }
@@ -31,6 +42,46 @@ public class Server {
         }catch(Exception e){
             e.printStackTrace();
             System.out.println("服务端启动失败！！");
+        }
+    }
+    // 该线程负责一个客户端的交互
+    class ClientHandler implements Runnable{
+        /**
+         * 该线程处理客户端的Socket
+         */
+        private Socket socket;
+        // 客户端的地址信息
+        private String host;
+        public ClientHandler(Socket socket){
+            this.socket = socket;
+            // 通过sockt可以获取计算机的地址信息
+            InetAddress address = socket.getInetAddress();
+            // 获取ip地址
+            host = address.getHostAddress();
+        }
+        @Override
+        public void run() {
+            try{
+                System.out.println(host + "上线了！");
+                // 获取输入流，读取远程发送过来的消息
+                InputStream in = socket.getInputStream();
+                InputStreamReader isr = new InputStreamReader(in,"UTF-8");
+                BufferedReader br = new BufferedReader(isr);
+                String message = null;
+                while((message = br.readLine()) != null){
+                    System.out.println(host + "说：" + message);
+                }
+            }catch(Exception e){
+                e.printStackTrace();
+            } finally {
+                // 处理当前客户端断开后的逻辑
+                System.out.println(host + "下线了！");
+                try{
+                    socket.close();
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
