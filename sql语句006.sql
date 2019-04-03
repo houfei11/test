6dd9ff11c54525c7a150890954873ed0ebec7297
houfei11 <46926730+houfei11@users.noreply.github.com>
2019-04-03 22:17:37 +0800

    sql语句006

---
+++ sql语句006.sql
@@ -0,0 +1,11 @@
+SELECT MAX(stuAge) FROM personal
+
+SELECT MIN(stuAge) FROM personal
+
+SELECT AVG(stuAge), SUM(stuAge) FROM personal
+
+SELECT COUNT(stuAge) FROM personal
+
+SELECT * FROM personal
+
+SELECT COUNT(*) FROM personal
