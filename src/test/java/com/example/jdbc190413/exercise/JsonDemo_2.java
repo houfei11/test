db430b9e50ed45aba5fe81e9f24ac904e588ea93
houfei <331488738@qq.com>
2019-05-21 22:02:20 +0800

    json

--- src/test/java/com/example/jdbc190413/exercise/JsonDemo_2.java
+++ src/test/java/com/example/jdbc190413/exercise/JsonDemo_2.java
@@ -1,12 +1,21 @@
 package com.example.jdbc190413.exercise;
 
 
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+import net.sf.json.JsonConfig;
+import net.sf.json.processors.JsonValueProcessor;
+import org.apache.commons.lang.time.DateFormatUtils;
+
+import java.util.Date;
+import java.util.HashMap;
 
 public class JsonDemo_2 {
     public static void main(String[] args) {
         doJsonToString();
+        doMapToJson();
+        getJsonArrayToString();
     }
     /**
      * json 转换字符串
@@ -18,6 +27,84 @@ public class JsonDemo_2 {
         System.out.println(jsonObject.toString());
         JSONArray array = new JSONArray();
         array.add(jsonObject);
-        System.out.println(array);
+        System.out.println(array.toString());
+        String string = array.toString();
+        System.out.println(string);
     }
+    /**
+     * map转换json对象
+     */
+    public static void doMapToJson(){
+        HashMap<String, Object> hashMap = new HashMap<>();
+        hashMap.put("userName", "zhangsan");
+        hashMap.put("password", "123456");
+        // 转换json数组
+        JSONArray object = JSONArray.fromObject(hashMap);
+        System.out.println(object);
+        // 转换json对象
+        JSONObject jsonObject = JSONObject.fromObject(hashMap);
+        System.out.println(jsonObject);
+    }
+    /**
+     * jsonObject中的一些方法和应用 todo ：未完成
+     */
+    public static void doJsonObjectHandle(){
+        Person person = new JsonDemo_2().new Person();
+        person.setAge(18);
+        person.setUserName("头晕");
+        person.getEndTime(new Date());
+        // 日期转换
+        JsonConfig config = new JsonConfig();
+        config.registerDefaultValueProcessor(java.util.Date.class, new JsonValueProcessor(){
+            @Override
+            public Object processArrayValue(Object arg0, JsonConfig arg1) {
+                return null;
+            }
+
+            @Override
+            public Object processObjectValue(String arg0, Object arg1, JsonConfig arg2) {
+                return DateFormatUtils.format(((Date)arg1, "yyyy-MM-dd");
+            }
+        });
+        JSONObject json = JSONObject.fromObject(person,config);
+        System.out.println(json);
+    }
+    public class Person{
+        private String userName;
+        private Integer age;
+        private Date endTime;
+
+        public String getUserName() {
+            return userName;
+        }
+
+        public void setUserName(String userName) {
+            this.userName = userName;
+        }
+
+        public Integer getAge() {
+            return age;
+        }
+
+        public void setAge(Integer age) {
+            this.age = age;
+        }
+
+        public Date getEndTime(Date date) {
+            return endTime;
+        }
+
+        public void setEndTime(Date endTime) {
+            this.endTime = endTime;
+        }
+    }
+    /**
+     * 把json数组里面的某个对象转换成字符串
+     */
+    public static void getJsonArrayToString(){
+        String str = "[{\"userName\":\"zhangsan\"},{\"userName\":\"lisi\"}]";
+        JSONArray object = JSONArray.fromObject(str);
+        System.out.println(object.getString(0));
+    }
+
 }
